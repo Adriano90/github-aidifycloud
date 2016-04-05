8e09f42b6207e18a95df6603fefce07c56da1a8c
Adriano90 <elamodeltinto@hotmail.com>
2016-04-05 23:22:31 +0200

    Added new activity

--- model/activitymapper.js
+++ model/activitymapper.js
@@ -8,10 +8,8 @@ const PUSH_EVENT = "PushEvent";
 const ISSUES_EVENT = "IssuesEvent";
 const PULL_REQUEST_EVENT = "PullRequestEvent";
 const COMMIT = "commit";
-const CLOSED = "closed";
-const OPENED = "opened";
-const REOPENED = "reopened";
 const CREATED = "created";
+const MERGED = "merged";
 
 module.exports.fromGitHub = function(gitHubActivity) {
 	
@@ -23,7 +21,7 @@ module.exports.fromGitHub = function(gitHubActivity) {
 		});
 		
 		activities.push(new Activity(gitHubActivity.id, PUSH_EVENT.toLowerCase(), CREATED,
-			gitHubActivity.actor.login, gitHubActivity.repo.name, gitHubActivity.created_at))
+			gitHubActivity.actor.login, gitHubActivity.repo.name, gitHubActivity.created_at));
 		
 		return activities;
 		
@@ -40,8 +38,9 @@ module.exports.fromGitHub = function(gitHubActivity) {
 			
 	} else if (PULL_REQUEST_EVENT == gitHubActivity.type) {
 		
+		let action = (!gitHubActivity.payload.pull_request.merged_at) ? gitHubActivity.payload.action : MERGED;
 		return new Activity(gitHubActivity.id, PULL_REQUEST_EVENT.toLowerCase(),
-			gitHubActivity.payload.action, gitHubActivity.payload.pull_request.user.login,
+			action, gitHubActivity.payload.pull_request.user.login,
 			gitHubActivity.repo.name, gitHubActivity.created_at, gitHubActivity.actor.login,
 			gitHubActivity.payload.pull_request.title, gitHubActivity.payload.pull_request.body);
 			
