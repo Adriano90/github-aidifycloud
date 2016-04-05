99025b8c3edd4b4c490911353c9923592688b071
Adriano90 <elamodeltinto@hotmail.com>
2016-04-05 22:18:19 +0200

    Changed activity mapping

--- model/activity.js
+++ model/activity.js
@@ -4,13 +4,14 @@
 
 class Activity {
 
-	constructor(id, type, action, user, repo, createdAt, title, body) {
+	constructor(id, type, action, user, repo, createdAt, eventUser, title, body) {
 		this.id = id;
 		this.type = type;
 		this.action = action;
 		this.user = user;
 		this.repo = repo;
 		this.createdAt = createdAt;
+		this.eventUser = eventUser;
 		this.title = title;
 		this.body = body;
 	}
--- model/activitymapper.js
+++ model/activitymapper.js
@@ -7,32 +7,43 @@ const MASTER = "refs/heads/master";
 const PUSH_EVENT = "PushEvent";
 const ISSUES_EVENT = "IssuesEvent";
 const PULL_REQUEST_EVENT = "PullRequestEvent";
+const COMMIT = "commit";
 const CLOSED = "closed";
 const OPENED = "opened";
 const REOPENED = "reopened";
+const CREATED = "created";
 
 module.exports.fromGitHub = function(gitHubActivity) {
 	
 	if (PUSH_EVENT == gitHubActivity.type && MASTER == gitHubActivity.payload.ref) {
 		
-		return gitHubActivity.payload.commits.map((elem) => {
-			return new Activity(gitHubActivity.id, "commit", "created",
+		let activities = gitHubActivity.payload.commits.map((elem) => {
+			return new Activity(gitHubActivity.id, COMMIT, CREATED,
 				elem.author.name, gitHubActivity.repo.name, gitHubActivity.created_at);
 		});
 		
+		activities.push(new Activity(gitHubActivity.id, PUSH_EVENT.toLowerCase(), CREATED,
+			gitHubActivity.actor.login, gitHubActivity.repo.name, gitHubActivity.created_at))
+		
+		return activities;
+		
 	} else if (ISSUES_EVENT == gitHubActivity.type) {
 		
+		if (!gitHubActivity.payload.issue.assignee) {
+			return null;
+		}
+		
 		return new Activity(gitHubActivity.id, ISSUES_EVENT.toLowerCase(),
-			gitHubActivity.payload.action, gitHubActivity.user.login,
-			gitHubActivity.repo.name, gitHubActivity.created_at,
-				gitHubActivity.payload.issue.title, gitHubActivity.payload.issue.body);
+			gitHubActivity.payload.action, gitHubActivity.payload.issue.assignee.login,
+			gitHubActivity.repo.name, gitHubActivity.created_at, gitHubActivity.actor.login,
+			gitHubActivity.payload.issue.title, gitHubActivity.payload.issue.body);
 			
 	} else if (PULL_REQUEST_EVENT == gitHubActivity.type) {
 		
 		return new Activity(gitHubActivity.id, PULL_REQUEST_EVENT.toLowerCase(),
 			gitHubActivity.payload.action, gitHubActivity.payload.pull_request.user.login,
-			gitHubActivity.repo.name, gitHubActivity.created_at,
-				gitHubActivity.payload.pull_request.title, gitHubActivity.payload.pull_request.body);
+			gitHubActivity.repo.name, gitHubActivity.created_at, gitHubActivity.actor.login,
+			gitHubActivity.payload.pull_request.title, gitHubActivity.payload.pull_request.body);
 			
 	}
 	
